5e91a47a141b108895d5221a8187933bfdc2a1b2
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-09-26 22:20:03 +0200

    question 2.3

--- src/hooks/use-cart.ts
+++ src/hooks/use-cart.ts
@@ -18,14 +18,35 @@ const useStore = create<CartData>(function (){
  *
  * @param product
  */
-export function addLine(product: ProductData) {}
+export function addLine(product: ProductData) {
+useStore((etat)=>{
+const LigneExistant=etat.lines.find((line)=>line.product.id===product.id);
+if(LigneExistant){
+    LigneExistant.qty+=1
+}
+else{
+    etat.lines.push({product,qty:1})
+}
+
+})
+
+}
 
 /**
  * Modifie une ligne produit du panier
  *
  * @param line
  */
-export function updateLine(line: ProductLineData) {}
+export function updateLine(line: ProductLineData) {
+    useStore((etat)=>{
+        const ligneExistant=etat.lines.find((l)=>l.product.id===line.product.id);
+        if(ligneExistant){
+            ligneExistant.qty=line.qty
+        }
+        
+        
+    })
+}
 
 /**
  * Supprime la ligne produit du panier
@@ -33,19 +54,31 @@ export function updateLine(line: ProductLineData) {}
  * @param productId
  * @returns
  */
-export function removeLine(productId: number) {}
+export function removeLine(productId: number) {
+    useStore((etat)=>{
+        etat.lines=etat.lines.filter((line)=>line.product.id!=productId)
+    })
+}
 
 /**
  * Vide le contenu du panier actuel
  */
-export function clearCart() {}
+export function clearCart() {
+    useStore((etat)=>{
+        etat.lines=[]
+    })
+}
 
 /**
  * Calcule le total d'une ligne du panier
  */
-export function computeLineSubTotal(line: ProductLineData): number {}
+export function computeLineSubTotal(line: ProductLineData): number {
+    return line.product.price*line.qty
+}
 
 /**
  * Calcule le total du panier
  */
-export function computeCartTotal(lines: ProductLineData[]): number {}
+export function computeCartTotal(lines: ProductLineData[]): number {
+    return lines.reduce((total,line)=>total+computeLineSubTotal(line),0)
+}
