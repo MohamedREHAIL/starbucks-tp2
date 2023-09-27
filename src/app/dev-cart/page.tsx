47828afc30fe50ee422bb573eb2072efb94865c3
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-09-27 13:09:28 +0200

    question 2.8

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -4,17 +4,13 @@ import { Button, ProductCardLayout, SectionContainer,ProductCartLine } from "../
 import {Card} from "tp-kit/components";
 import {addLine, computeCartTotal, computeLineSubTotal, removeLine, updateLine} from "../../hooks/use-cart";
 import {useEffect} from "react";
+import {Cart} from "../../components/cart";
 import {useStore} from "../../hooks/use-cart";
 
 
 export default function DevCartPage() {
+    console.log("rendu page");
     const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
-    // const { lines } = useStore();
-    const lines = useStore((state) => state.lines)
-
-    useEffect(() => {
-        console.log(lines);
-    }, [lines]);
 
     return (
         <SectionContainer
@@ -33,32 +29,12 @@ export default function DevCartPage() {
 
                 ))}
             </section>
-            {/*<pre>{JSON.stringify(lines, null, 2)}</pre>*/}
+            
 
             {/* /Produits */}
 
             {/* Panier */}
-
-                <section className="w-full lg:w-1/3 space-y-8">
-                    <h2>Mon Panier</h2>
-                    {lines.map((product)=>(
-                        <>
-                            <ProductCartLine key={product.product.id} product={product.product} qty={product.qty} onQtyChange={(qty)=>updateLine({...product, qty})} onDelete={()=>removeLine(product.product.id)} ></ProductCartLine>
-
-                        </>
-
-                    ))}
-                    <div className="flex justify-between">
-                        <h3>Total</h3>
-                        <div>{computeCartTotal(lines.map(p=>p))}</div>
-                    </div>
-                    <Button variant={"primary"} fullWidth>Commander</Button>
-                    <Button variant={"outline"} fullWidth>Vider le panier</Button>
-
-
-                </section>
-
-
+            <Cart></Cart>
             {/* /Panier */}
         </SectionContainer>
     );
---
+++ src/components/cart.tsx
@@ -0,0 +1,37 @@
+import {computeCartTotal, removeLine, updateLine, useStore} from "../hooks/use-cart";
+import {Button, Card} from "tp-kit/components";
+import {ProductCartLine} from "tp-kit/components/products";
+import {useEffect} from "react";
+
+export function Cart(){
+    const lines = useStore((state) => state.lines)
+
+    useEffect(() => {
+        console.log(lines);
+    }, [lines]);
+
+    return(<>
+
+        <Card>
+            <section className="w-full lg:w-1/3 space-y-8">
+                <h2>Mon Panier</h2>
+                {lines.map((product)=>(
+                    <>
+                        <ProductCartLine key={product.product.id} product={product.product} qty={product.qty} onQtyChange={(qty)=>updateLine({...product, qty})} onDelete={()=>removeLine(product.product.id)} ></ProductCartLine>
+
+                    </>
+
+                ))}
+                <div className="flex justify-between">
+                    <h3>Total</h3>
+                    <div>{computeCartTotal(lines.map(p=>p))}</div>
+                </div>
+                <Button variant={"primary"} fullWidth>Commander</Button>
+                <Button variant={"outline"} fullWidth>Vider le panier</Button>
+
+
+            </section>
+        </Card>
+
+    </>)
+}
