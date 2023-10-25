20a8cc3eadfb7ec768c1f4f823c953ef269954ca
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-10-25 17:28:26 +0200

    tp3 question 3.3 à finir

--- src/app/[categorySlug]/page.tsx
+++ src/app/[categorySlug]/page.tsx
@@ -3,21 +3,37 @@ import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../../components/product-list";
 import { NextPageProps } from "../../types";
 import { Metadata } from "next";
+import prisma from "../../utils/prisma";
 const category = PRODUCTS_CATEGORY_DATA[0];
+
 
 type Props = {
   categorySlug: string;
 };
 
+  async function getCategory() {
+  const categorie = await prisma.productCategory.findFirst({
+
+    include:{
+      products:true
+    }
+  })
+  return categorie
+}
+
 export async function generateMetadata({ params, searchParams} : NextPageProps<Props>) : Promise<Metadata> {
+  {console.log("ddddd")}
+  // const category = await getCategory
   return {
     title: category.name,
     description: `Trouvez votre inspiration avec un vaste choix de boissons Starbucks parmi nos produits ${category.name}`
   }
 }
 
-export default function CategoryPage({params}: NextPageProps<Props>) {
+export default async function CategoryPage({params}: NextPageProps<Props>) {
+  {console.log("ddddd")}
   return <SectionContainer>
+   
     <BreadCrumbs 
       items={[
         {
